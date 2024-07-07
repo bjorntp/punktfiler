bbe52961d96be09ecb55e01caefe6f144057d519
Björn Tenje Persson <bjorn@tenje.se>
2024-07-07 10:49:17 +0200

    added if else

--- configs/polybar/laptop/scripts/bluetooth.sh
+++ configs/polybar/laptop/scripts/bluetooth.sh
@@ -6,6 +6,7 @@ else
   if [ $(bluetoothctl devices Connected | wc -c) -ne 0 ]
   then 
     echo $(bluetoothctl devices Connected | sed -E 's/Device ([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})//')
+  else
+    echo "ON"
   fi
-  echo "ON"
 fi
