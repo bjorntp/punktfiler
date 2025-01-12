48c88c18c810bd7d8161ee30ac7c2749bb792aa0
Björn Tenje Persson <bjorn@tenje.se>
2025-01-12 12:55:14 +0100

    wayland install files

---
+++ laptop_packages_wayland.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo pacman -S --noconfirm --disable-download-timeout sudo gnome-keyring seahorse git zsh bluez curl networkmanager discord ranger vim neovim firefox vlc gimp signal-desktop wget bluez-utils libnotify rofi kitty ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-meslo-nerd qt5-graphicaleffects qt5-svg qt5-quickcontrols2 xorg-xrandr thunderbird thunderbird brightnessctl fastfetch ripgrep npm gdm zathura zathura-cb zathura-pdf-mupdf pdfgrep texlive texlive-lang less gcc hyprland hyprpaper networkmanager waybar
