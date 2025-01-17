6ebb552306afccc6a104ae22afabb0d83ccd3e83
Björn Tenje Persson <bjorn@tenje.se>
2025-01-17 10:10:51 +0100

    Added screenshots to wayland Wms

--- package_directories/desktop_packages_hyprland.sh
+++ package_directories/desktop_packages_hyprland.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-
-sudo pacman -S --noconfirm --disable-download-timeout hyprland hyprpaper networkmanager waybar wl-clipboard
+sudo pacman -S --noconfirm --disable-download-timeout hyprland hyprpaper networkmanager waybar wl-clipboard grim slurp
--- package_directories/laptop_packages_hyprland.sh
+++ package_directories/laptop_packages_hyprland.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo pacman -S --noconfirm --disable-download-timeout brightnessctl hyprland hyprpaper networkmanager waybar wl-clipboard
+sudo pacman -S --noconfirm --disable-download-timeout brightnessctl hyprland hyprpaper networkmanager waybar wl-clipboard grim slurp
