e404533a6c4e26ade5e50680c819b649eef1a2c8
Björn Tenje Persson <bjorn@tenje.se>
2025-05-12 10:52:04 +0200

    added hyprlock to laptop config

--- package_directories/laptop_packages_hyprland.sh
+++ package_directories/laptop_packages_hyprland.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo pacman -S --noconfirm --disable-download-timeout brightnessctl hyprland hyprpaper networkmanager waybar wl-clipboard grim slurp
+sudo pacman -S --noconfirm --disable-download-timeout brightnessctl hyprland hyprpaper networkmanager waybar wl-clipboard grim slurp hyprlock
