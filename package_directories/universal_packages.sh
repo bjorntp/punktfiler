c4461014489a390f6a8c4eecce9cd9a5796f2029
Björn Tenje Persson <bjorn@tenje.se>
2025-05-19 11:07:36 +0200

    Changes so linkes dirs work better in arch

--- package_directories/universal_packages.sh
+++ package_directories/universal_packages.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --noconfirm --disable-download-timeout sudo git zsh bluez curl networkmanager discord ranger vim neovim firefox vlc gimp signal-desktop wget bluez-utils libnotify kitty ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-meslo-nerd qt5-graphicaleffects qt5-svg qt5-quickcontrols2 xorg-xrandr thunderbird fastfetch ripgrep npm gdm zathura zathura-cb zathura-pdf-mupdf pdfgrep texlive texlive-langeuropean less gcc mlocate man gnome-keyring seahorse rofi bluetui tree jdk-openjdk python lazygit otf-firamono-nerd ttf-fira-code ttf-fira-mono
+sudo pacman -S --noconfirm --disable-download-timeout sudo git bluez curl networkmanager discord ranger vim neovim firefox vlc gimp signal-desktop wget bluez-utils libnotify kitty  fastfetch ripgrep npm zathura zathura-cb zathura-pdf-mupdf pdfgrep texlive texlive-langeuropean less gcc mlocate man gnome-keyring seahorse rofi bluetui tree jdk-openjdk python lazygit 
