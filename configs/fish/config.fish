905ce20475d038783f4b9da5f780473d6d432a0e
Björn Tenje Persson <bjorn@tenje.se>
2025-05-20 10:44:57 +0200

    fish aliases

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,5 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias ls='eza --no-time'
-end
 
+    # Add ~/.local/bin to PATH
+    set -x PATH $HOME/.local/bin $PATH
+
+    # Enable vim key bindings
+    fish_vi_key_bindings
+end
