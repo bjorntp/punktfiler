e2322eb8bbbc4cb04e5e18d6bc56ce12bd611279
Björn Tenje Persson <bjorn@tenje.se>
2025-05-21 09:20:09 +0200

    bat alias to fish

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     alias ls='eza --no-time'
     alias lg='lazygit'
+    alias cat='bat'
 
     # Add ~/.local/bin to PATH
     set -x PATH $HOME/.local/bin $PATH
