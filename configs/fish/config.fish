ca8eaae4e4d25d8ed4ac97c4b0db87f8c4c5fb22
Björn Tenje Persson <bjorn@tenje.se>
2025-05-21 08:57:13 +0200

    installed z plugin for fish

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias ls='eza --no-time'
+    alias lg='lazygit'
 
     # Add ~/.local/bin to PATH
     set -x PATH $HOME/.local/bin $PATH
